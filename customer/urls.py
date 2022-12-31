447462e27cbf0c3978c5a8fd64ce59f21a45bab5
rvale01 <rvale09@gmail.com>
2022-12-31 01:15:09 +0000

    init project with all templates and folder structure

---
+++ customer/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("payment", views.payment, name="payment"),
+    path("booking-review/<str:showing_id>", views.booking_review, name="booking_review"), #booking confirmation with id of booking, id of customer
+    path("showings/<str:date>/<str:time>", views.showings_list, name="showings_list"), 
+    path("showing_details/<str:film_id>", views.showing_details, name="showing details"), 
+    path("select-date-showings", views.date_selection),
+    path("select_tickets/<str:showing_id>", views.select_tickets),
+]
